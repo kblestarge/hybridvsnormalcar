81f31fb4c80f746ce4030ce6d34f69772e7f4cba
Kevin LeStarge <kblestarge@gmail.com>
2015-06-26 16:45:35 -0600

    functionality is pretty much done

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var hybrid = {
-	type:'h'
+	type:'h',
 	initialCost:'',
 	mpg:'',
 	resale:''
 }
 
 var normal = {
+	type:'n',
 	initialCost:'',
 	mpg:'',
 	resale:''
@@ -16,30 +17,58 @@ var milesDriven;
 
 function fight() {
 
-	fillHybrid();
-	fillNormal();
+	fillCar(hybrid);
+	console.log("hybrid:",hybrid);
+
+	fillCar(normal);
+	console.log("normal:",normal);
 
 	gasCost = document.getElementById('gas-cost').value;
 	milesDriven = document.getElementById('num-miles').value;
-	fillCar('n');
-	fillCar(hybrid);
+
+	hybridCost = cost(hybrid, gasCost, milesDriven);
+	console.log("hybridCost:",hybridCost);
+
+	normalCost = cost(normal, gasCost, milesDriven);
+	console.log("normalCost:",normalCost);
+
+	hybridGallons = fuelConsumed(hybrid, milesDriven)
+	console.log("hybridGallons:",hybridGallons);
+
+	normalGallons = fuelConsumed(normal, milesDriven)
+	console.log("normalGallons:",normalGallons);
+
+	outputResults(hybrid, hybridCost, hybridGallons);
+	outputResults(normal, normalCost, normalGallons);
 }
 
 function fillCar(car) {
 	
-	car.mpg = document.getElementById('').value;
+	car.initialCost = document.getElementById(car.type + '-initial-cost').value;
+	car.mpg = document.getElementById(car.type + '-mpg').value;
+	car.resale = document.getElementById(car.type + '-resale').value;
 }
 
-function fillHybrid() {
-	hybrid.initialCost = document.getElementById(car.type + '-initial-cost').value;
-	hybrid.mpg = document.getElementById('h-mpg').value;
-	hybrid.resale = document.getElementById('h-resale').value;
-	console.log("hybrid:",hybrid);
+function cost(car, gas, miles) {
+
+	var depreciation = car.initialCost - car.resale;
+	var gallons = fuelConsumed(car, miles);
+	var costOfGas = gallons * gas;
+	var totalCost = costOfGas + depreciation;
+
+	return totalCost;
 }
 
-function fillNormal() {
-	normal.initialCost = document.getElementById('n-initial-cost').value;
-	normal.mpg = document.getElementById('n-mpg').value;
-	normal.resale = document.getElementById('n-resale').value;
-	console.log("normal:",normal);
+function fuelConsumed(car, miles) {
+	var gallons = miles / car.mpg;
+	return gallons;
+}
+
+function outputResults(car, cost, gallons) {
+
+	var gallonElm = document.getElementById(car.type + '-results').getElementsByTagName('p')[1];
+	var costElm = document.getElementById(car.type + '-results').getElementsByTagName('p')[2];
+
+	gallonElm.innerHTML = gallons;
+	costElm.innerHTML = cost;
 }
