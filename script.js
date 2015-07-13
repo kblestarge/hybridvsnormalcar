7cb60962086c28efb34deb3e690f97c3dbd4cdae
Kevin LeStarge <kblestarge@gmail.com>
2015-07-13 14:58:45 -0600

    Complete, but not too dynamic with different sized browsers...

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ var hybrid = {
 	type:'h',
 	initialCost:'',
 	mpg:'',
-	resale:''
+	resale:'',
+	totalCost:'',
+	gasUsed:''
 }
 
 var normal = {
 	type:'n',
 	initialCost:'',
 	mpg:'',
-	resale:''
+	resale:'',
+	totalCost:'',
+	gasUsed:''
 }
 
+var battleType;
 var gasCost;
 var milesDriven;
 
@@ -20,32 +25,34 @@ $(document).ready(function() {
 
 	$('button').click(function(){
 
-		// normal.initialCost = document.getElementById(normal.type + '-initial-cost').value;
-		// normal.mpg = document.getElementById(normal.type + '-mpg').value;
-		// normal.resale = document.getElementById(normal.type + '-resale').value;
+		battleType = $(".battle input[type='radio']:checked").val();
 
-		// hybrid.initialCost = document.getElementById(hybrid.type + '-initial-cost').value;
-		// hybrid.mpg = document.getElementById(hybrid.type + '-mpg').value;
-		// hybrid.resale = document.getElementById(hybrid.type + '-resale').value;
+		normal.initialCost = document.getElementById(normal.type + '-initial-cost').value;
+		normal.mpg = document.getElementById(normal.type + '-mpg').value;
+		normal.resale = document.getElementById(normal.type + '-resale').value;
 
-		// gasCost = document.getElementById('gas-cost').value;
-		// milesDriven = document.getElementById('num-miles').value;
+		hybrid.initialCost = document.getElementById(hybrid.type + '-initial-cost').value;
+		hybrid.mpg = document.getElementById(hybrid.type + '-mpg').value;
+		hybrid.resale = document.getElementById(hybrid.type + '-resale').value;
 
-		// if(gasCost == '' || milesDriven == '' || normal.initialCost == '' || normal.mpg == '' || normal.resale == '' || hybrid.initialCost == '' || hybrid.mpg == '' || hybrid.resale== '') { //check inputs
-		// 	alert('Don\'t hold back, my friend. Fill up the stats!');
-		// }else { //go
+		gasCost = document.getElementById('gas-cost').value;
+		milesDriven = document.getElementById('num-miles').value;
+
+		if(gasCost == '' || milesDriven == '' || normal.initialCost == '' || normal.mpg == '' || normal.resale == '' || hybrid.initialCost == '' || hybrid.mpg == '' || hybrid.resale== '' || battleType == null) { //check inputs
+			alert('Don\'t hold back, my friend. Fill up the stats!');
+		}else { //go
 
 			fillCar(hybrid);
 			fillCar(normal);	
 
-			hybridCost = cost(hybrid, gasCost, milesDriven);
-			normalCost = cost(normal, gasCost, milesDriven);
+			hybrid.totalCost = cost(hybrid, gasCost, milesDriven);
+			normal.totalCost = cost(normal, gasCost, milesDriven);
 
-			hybridGallons = fuelConsumed(hybrid, milesDriven)
-			normalGallons = fuelConsumed(normal, milesDriven)
+			hybrid.gasUsed = fuelConsumed(hybrid, milesDriven)
+			normal.gasUsed = fuelConsumed(normal, milesDriven)
 
-			outputResults(hybrid, hybridCost, hybridGallons);
-			outputResults(normal, normalCost, normalGallons);
+			outputResults(hybrid, hybrid.totalCost, hybrid.gasUsed);
+			outputResults(normal, normal.totalCost, normal.gasUsed);
 
 			$('.red-car').animate({
 				marginLeft: "35%"
@@ -57,15 +64,14 @@ $(document).ready(function() {
 
 			$('.explosion').delay(100).fadeIn( "medium" );
 
-			//calculate winner, prob an if
-			$('#h-results').css('background-color', '#C98910');
-			$('#n-results').css('background-color', '#A8A8A8');
+			//Assign winner
+			assignWinner(battleType);
 
 			$('.popup').delay(700).animate({
 				height: "toggle",
 				fontSize: "toggle"
 			},500);
-		// }
+		}
 	})
 
 	$('.popup').click(function(){
@@ -114,6 +120,43 @@ function outputResults(car, cost, gallons) {
 	var gallonElm = document.getElementById(car.type + '-results').getElementsByTagName('span')[0];
 	var costElm = document.getElementById(car.type + '-results').getElementsByTagName('span')[1];
 
-	gallonElm.innerHTML = gallons;
-	costElm.innerHTML = cost;
+	gallonElm.innerHTML = Math.round(gallons * 10) / 10;
+	costElm.innerHTML = '$' + Math.round(cost);
 }
+function assignWinner(battleType){
+
+	if(battleType == 'gas'){
+		if(hybrid.gasUsed <= normal.gasUsed){
+			$('#h-results').css('background-color', '#C98910');//gold
+			$('#h-results img').attr('src', 'img/1st.png');
+			$('#n-results img').attr('src', '');
+			$('.h-car-fin img').attr('src', 'img/carRed.png');
+			$('.n-car-fin img').attr('src', 'img/carYellowBroken.png');
+			$('#n-results').css('background-color', '#A8A8A8');//silver
+		}else{
+			$('#h-results').css('background-color', '#A8A8A8');//silver
+			$('#n-results img').attr('src', 'img/1st.png');
+			$('#h-results img').attr('src', '');
+			$('.n-car-fin img').attr('src', 'img/carYellow.png');
+			$('.h-car-fin img').attr('src', 'img/carRedBroken.png');
+			$('#n-results').css('background-color', '#C98910');//gold
+		}
+	}else{ //cost
+		if(hybrid.totalCost <= normal.totalCost){
+			$('#h-results').css('background-color', '#C98910');//gold
+			$('#h-results img').attr('src', 'img/1st.png');
+			$('#n-results img').attr('src', '');
+			$('.h-car-fin img').attr('src', 'img/carRed.png');
+			$('.n-car-fin img').attr('src', 'img/carYellowBroken.png');
+			$('#n-results').css('background-color', '#A8A8A8');//silver
+		}else{
+			$('#h-results').css('background-color', '#A8A8A8');//silver
+			$('#n-results img').attr('src', 'img/1st.png');
+			$('#h-results img').attr('src', '');
+			$('.n-car-fin img').attr('src', 'img/carYellow.png');
+			$('.h-car-fin img').attr('src', 'img/carRedBroken.png');
+			$('#n-results').css('background-color', '#C98910');//gold
+		}
+	}
+}
+			
